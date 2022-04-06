b2ab2ace8773b5f73237dee6b30c95d110c1a328
NikitaSemenov2708 <92303348+NikitaSemenov2708@users.noreply.github.com>
2022-04-06 20:12:36 +0300

    решил до создания нового массива

--- Program.cs
+++ Program.cs
@@ -7,3 +7,37 @@
 ["1234", "1567", "-2", "computer science"] -> ["-2"]
 
 ["Russia", "Denmark", "Kazan"] -> [] */
+
+ void My_array(string[]arg)
+{
+    string result=string.Empty;
+    for (int i = 0; i < arg.Length; i++)
+    {
+        result=arg[i];
+        int length= arg[i].Length;
+        if (length<3)
+        {
+            
+            
+        }
+        
+        
+            
+    }
+}
+string[] argum = {"hello", "2", "world", ":-)"};
+My_array(argum); 
+
+/* void FineWords (string alphabet, char[] word, int length=0  )
+{
+    if(length==word.Length)
+    {
+        Console.WriteLine($" {new string(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length]=alphabet[i];
+        FineWords(alphabet, word, length+1);
+    }
+}
+FineWords("aисв", new char[2]); */
